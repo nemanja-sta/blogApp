c89b89d4b14f8f17dc2f5b0cfc655e3ff39291c3
Nemanja Stanisavljevic <nemanja.sta1@gmail.com>
2020-11-25 14:11:52 +0100

    routes updated

--- models/post.js
+++ models/post.js
@@ -1,23 +1,23 @@
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const post = {
-    title: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true 
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+   title: {
+      type: String,
+      required: true
+   },
+   body: {
+      type: String,
+      required: true
+   },
+   author: {
+      type: String,
+      required: true
+   },
+   date: {
+      type: Date,
+      default: Date.now
+   }
 };
 
 const PostSchema = new Schema(post);
--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const router = express.Router;
+const express = require("express");
 require("dotenv").config(); // for loading environment variables
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const passport = require("passport");
+const path = require("path");
+const users = require("./routes/api/users");
+
 const app = express();
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.post("/user", (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
-
-app.use("/api/users/", require("./routes/api/user"));
+app.use(bodyParser.json());
 
 //Database Connect
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useUnifiedTopology: true })
 .then(() => console.log("Mongo Connection successful"))
 .catch(err => console.log("err"));
+
+mongoose.set("useFindAndModify", false);
+mongoose.Promise = global.Promise;
+
+app.use(passport.initialize());
+require("./middleware/passport")(passport);
+app.use("/api/users/", require("./routes/api/users"));
+app.use("/api/posts/", require("./routes/api/posts"));
+
 
 //Port Connect
 app.listen(5000, () => {
