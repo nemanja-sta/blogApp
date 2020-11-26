7796e51900252b3e2689ee7241004c2d20182abb
Nemanja Stanisavljevic <nemanja.sta1@gmail.com>
2020-11-26 21:05:18 +0100

    client initialize

--- server.js
+++ server.js
@@ -27,6 +27,5 @@ app.use("/api/posts/", require("./routes/api/posts"));
 
 
 //Port Connect
-app.listen(5000, () => {
-  console.log(`Server up and running on port 5000`);
-});    
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));  
